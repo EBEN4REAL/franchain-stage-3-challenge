f1fd8ed84e75b3bdde29ab6bdbf90c7fb27de433
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2024-05-19 02:45:09 +0100

    chore: update cart

--- franchain-ui/src/pages/Home/components/ProductItem/useProductItem.ts
+++ franchain-ui/src/pages/Home/components/ProductItem/useProductItem.ts
@@ -1,21 +1,11 @@
-import { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 import { toast } from 'react-toastify';
 
 import { useRootLayoutContext } from '../../../../layout/RootLayout';
 import { addToCart } from '../../../../services/Cart.service';
-import { getNumberOfItemsInCart } from '../../Home.utils';
 
 export default function useProductItem(id: number) {
-	const { cartList, refetch } = useRootLayoutContext();
-
-	const [numberOfItemsInCart, setNumberOfItemsInCart] = useState(0);
-
-	useEffect(() => {
-		const initNumberOfItemsInCart = getNumberOfItemsInCart(cartList, id);
-
-		setNumberOfItemsInCart(initNumberOfItemsInCart);
-	}, [cartList, id]);
+	const { refetch } = useRootLayoutContext();
 
 	const { mutate: onAddToCart } = useMutation({
 		mutationFn: addToCart,
@@ -25,9 +15,8 @@ export default function useProductItem(id: number) {
 
 	function onAddToCartSuccessful(data: { message: string }) {
 		refetch()
-		setNumberOfItemsInCart((prev) => prev + 1);
 		toast.success(data.message);
 	}
 
-	return { onAddToCart, numberOfItemsInCart };
+	return { onAddToCart };
 }
